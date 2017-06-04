80b38bdeb36833d4528ec1ef2a7e6c9ec92f7f19
maniatsig <tsigarida.mania@gmail.com>
2017-06-04 16:14:07 +0300

    Update ispend.js

--- ispend.js
+++ ispend.js
@@ -1,51 +1,59 @@
+//function to hide an element
 function hide(element){
 	
 					document.getElementById(element).style.display = "none";
 }
 
+//function to show an element
 function show(element){
 	
 					document.getElementById(element).style.display = "block";
 }
 
-function insertdata()
+//function to show specific content
+function show_content(selection)
 				{
-			
-							var id = window.localStorage.getItem('lastid');
-							
-							var spendammount = document.forms['clientForm'].sammount.value; //ammount
-							var category = document.forms['clientForm'].categ.value; //category
-							var necessary = document.forms['clientForm'].nec.checked; //anagaio
-							var userdata = {'Category_selection': category, 'Ammount_selection': spendammount, 'Necessary_selection': necessary };
-							
-							
-							if(spendammount != "")
-							{
-								if ((typeof(Storage) !== "undefined")) 
-								{
-										for (var prop in userdata) {
-											console.log('  ' + prop + ': ' + userdata[prop]);
-									}
-									localStorage.setItem(id, JSON.stringify(userdata));	
-									edit_counters(id, "add");									
-									id++;
-									localStorage.setItem('lastid',id);
-								}
-								
-							}
-							
-							clear_form();
+					
+					set_counters();
+					hide("message_box");
+					if(selection=="new")
+					{
+						document.getElementById("nav_new").classList.add('selected');
+						document.getElementById("nav_stats").classList.remove('selected');
+						document.getElementById("nav_edit").classList.remove('selected');
+						show("new");
+						hide("stats");
+						hide("edit");	
+					}
+					else if (selection=="stats")
+					{
+						document.getElementById("nav_stats").classList.add('selected');
+						document.getElementById("nav_new").classList.remove('selected');
+						document.getElementById("nav_edit").classList.remove('selected');
+						hide("new");
+						show("stats");
+						hide("edit");
+						datastats();
+					}
+					else
+					{
+						document.getElementById("nav_edit").classList.add('selected');
+						document.getElementById("nav_new").classList.remove('selected');
+						document.getElementById("nav_stats").classList.remove('selected');
+						hide("new");
+						hide("stats");
+						show("edit");
+						dataedit();
+					}
+					
 				}
-				
+
+//function to set all counters to 0 for first time				
 function set_counters()
 				{
-					
-					
-						
 						
 					if (window.localStorage.getItem('lastid')== null){
 						localStorage.setItem('lastid',1);
-						//console.log(window.localStorage.getItem('lastid') + "last id null");
 					}
 					
 					if(window.localStorage.getItem("food_sumammount_nec") == null)
@@ -77,12 +85,45 @@ function set_counters()
 					if(window.localStorage.getItem("other_sumammount_not_nec") == null)
 										window.localStorage.setItem("other_sumammount_not_nec",0);
 				}
+
+//function to save user's data
+function insertdata()
+				{
+			
+							var id = window.localStorage.getItem('lastid');
+							
+							var spendammount = document.forms['clientForm'].sammount.value;
+							var category = document.forms['clientForm'].categ.value;
+							var necessary = document.forms['clientForm'].nec.checked;
+							var new_record = {'Category_selection': category, 'Ammount_selection': spendammount, 'Necessary_selection': necessary };
+							
+							
+							if(spendammount != "")
+							{
+								if ((typeof(Storage) !== "undefined")) 
+								{
+										for (var prop in new_record) {
+											console.log('  ' + prop + ': ' + new_record[prop]);
+									}
+									localStorage.setItem(id, JSON.stringify(new_record));	
+									edit_counters(id, "add");									
+									id++;
+									localStorage.setItem('lastid',id);
+								}
+								
+							}
+							
+							clear_form();
+				}
+				
+//function to clear form's data
 function clear_form()
 				{
 					
 					document.getElementById("clientForm").reset();
 				}				
-					
+
+//function to clear all local storage data			
 function clear_all()
 				{
 					localStorage.clear();
@@ -90,47 +131,8 @@ function clear_all()
 					
 					
 				}				
-				
-function show_content(selection)
-				{
-					
-					set_counters();
-					hide("message_box");
-					if(selection=="new")
-					{
-						document.getElementById("nav_new").classList.add('selected');
-						document.getElementById("nav_stats").classList.remove('selected');
-						document.getElementById("nav_edit").classList.remove('selected');
-						show("new");
-						hide("stats");
-						hide("edit");
-						
-						
-					}
-					else if (selection=="stats")
-					{
-						document.getElementById("nav_stats").classList.add('selected');
-						document.getElementById("nav_new").classList.remove('selected');
-						document.getElementById("nav_edit").classList.remove('selected');
-						hide("new");
-						show("stats");
-						hide("edit");
-						datastats();
-						
-					}
-					else
-					{
-						document.getElementById("nav_edit").classList.add('selected');
-						document.getElementById("nav_new").classList.remove('selected');
-						document.getElementById("nav_stats").classList.remove('selected');
-						hide("new");
-						hide("stats");
-						show("edit");
-						dataedit();
-					}
-					
-				}
 
+//function to show all user's entries in a column chart
 function datastats()
 				{
 					
@@ -151,6 +153,7 @@ function datastats()
 					var view = new google.visualization.DataView(data);
 						
 					var options = {
+
 							    colors: ['#34675C', '#8FBC8F'],
 								backgroundColor: '#E6FFF2',
 								bar: {groupWidth: "80%"},
@@ -163,11 +166,13 @@ function datastats()
 					chart.draw(view, options);
 					}
 						
-				}					
+				}		
+
+//function to show all user's entries and let the user delete them				
 function dataedit()
 				{
 					
-					var all_data = "";
+					var all_data="";
 					var record;
 					var li_class;
 					
@@ -196,8 +201,6 @@ function dataedit()
 											{
 												all_data = all_data + "<li class=\" " + li_class + "\" onclick=\"delete_data(" + i + ")\">" + record.Category_selection + " έξοδα: " + record.Ammount_selection + "€" + "<img src=\"assets/close.svg\"</li>";
 											}
-									
-											
 											
 										}
 									}
@@ -206,7 +209,8 @@ function dataedit()
 								
 	
 				}	
-				
+
+//function to remove specific user's record from local storage after confirmation			
 function delete_data(record_id)
 {
 					var record = JSON.parse(window.localStorage.getItem(record_id));
@@ -214,24 +218,26 @@ function delete_data(record_id)
 					hide("edit");
 					document.getElementById("delete_record").innerHTML = record.Category_selection + " έξοδα: " + record.Ammount_selection + "€";
 					
-					document.getElementById("delete_yes").addEventListener("click", function(){
+					document.getElementById("delete_yes").onclick = function(){
 										
-										show("edit");
+										
 										edit_counters(record_id, "remove");
 										localStorage.removeItem(record_id);
 										dataedit();
+										show("edit");
 										
-									});
-					document.getElementById("delete_no").addEventListener("click", function(){
+									};
+					document.getElementById("delete_no").onclick = function(){ 
 										
-									show("edit");
-									dataedit();
+										show("edit");
+										dataedit();
 									
-									});
+									};
 									
 									
 }
-				
+
+//function to renew counters after a deletion or an insertion				
 function edit_counters(record_id, symbol)
 {
 					var record = JSON.parse(window.localStorage.getItem(record_id));
@@ -304,7 +310,7 @@ function edit_counters(record_id, symbol)
 					else
 					{
 		
-				console.log(record);
+				
 										if(record.Necessary_selection)
 										{ 
 												if(record.Category_selection == "Φαγητό/Ποτό")
